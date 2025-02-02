9168cea12072f09eb0827fac66539c2dd5ba940c
Neuralist <rimedheart@yandex.ru>
2025-02-02 20:54:51 +0300

    Added port listening for render.com

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 """
 Импорт зависимостей
 """
+import os
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, F
@@ -82,4 +83,5 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 3000))
     asyncio.run(main())
